2683eecdb67d3b8f45417e10b2e890248477d3f3
lemon-cake123 <faizan.athaurrahman@gmail.com>
2021-12-07 12:46:06 +0700

    Day 7

---
+++ 2021/day 7/aoc202107.py
@@ -0,0 +1,89 @@
+# aoc_template.py
+'''
+Note : this template was created during day 7 2021,
+so some days may not use this template
+'''
+import pathlib
+import sys
+
+def parse(puzzle_input):
+    """Parse input"""
+    return [int(data) for data in puzzle_input.split(',')]
+
+def part1(data):
+    """Solve part 1"""
+    fuel = []
+    current_fuel = 0
+    possible_alingn = list(range(0,max(data) + 1))
+    #print(possible_alingn)
+    for pos in possible_alingn:
+        current_fuel = 0
+        for crab_pos in data:
+            #print((pos,crab_pos))
+            if pos < crab_pos:
+                current_fuel += crab_pos - pos
+                #print((pos,crab_pos,crab_pos - pos))
+                
+            else:
+                current_fuel += pos - crab_pos
+                #print((pos,crab_pos,pos - crab_pos))
+                
+        #print(current_fuel)
+                
+        fuel.append(current_fuel)
+    
+    #print(min(fuel))
+    #print(fuel)
+    return min(fuel)
+        
+
+
+    
+
+def part2(data):
+    """Solve part 2"""
+    current_fuel = 0
+    fuel = []
+    possible_alingn = list(range(0,max(data) + 1))
+    for pos in possible_alingn:
+        current_fuel = 0
+        for crab_pos in data:
+            #print((pos,crab_pos))
+            if pos < crab_pos:
+                range_needed = crab_pos - pos
+                current_fuel += sum(range(1,range_needed + 1))
+                #print((pos,sum(range(1,range_needed)),range_needed))
+                
+                #print((pos,crab_pos,crab_pos - pos))
+                
+            else:
+                range_needed = pos - crab_pos
+                current_fuel += sum(range(1,range_needed + 1))
+                
+                #print((pos,sum(range(1,range_needed)),range_needed))
+                
+                #print((pos,crab_pos,pos - crab_pos))
+                
+        #print(current_fuel)
+                
+        fuel.append(current_fuel)
+    
+    #print(min(fuel))
+    #print(fuel)
+    return min(fuel)
+    
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input"""
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    return solution1, solution2
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        solutions = solve(puzzle_input)
+        print("\n".join(str(solution) for solution in solutions))
---
+++ 2021/day 7/test_aoc202107.py
@@ -0,0 +1,29 @@
+import pathlib
+import pytest
+import aoc202107 as aoc #change aoc_template to name of file
+'''
+a file used to test aoc solutions
+'''
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+@pytest.fixture
+def example1():
+    puzzle_input = (PUZZLE_DIR / "example_input.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
+
+
+
+def test_parse_example1(example1):
+    """Test that input is parsed properly"""
+    assert example1 == [16,1,2,0,4,2,7,1,2,14]
+
+
+def test_part1_example1(example1):
+    """Test part 1 on example input"""
+    assert aoc.part1(example1) == 37
+
+
+def test_part2_example1(example1):
+    """Test part 2 on example input"""
+    assert aoc.part2(example1) == 168
